d4e02fc7bb7dcd929e9b0d5e68f7f0cbba0e518e
Joel Preas <JoelPreas@gmail.com>
2019-09-10 20:29:59 -0400

    bunch of fixes, readme update

--- make_yousirs_venv.sh
+++ make_yousirs_venv.sh
@@ -1,5 +1,6 @@
 py3venv_dir="${PWD}/py3venv"
 python3 -m venv ${py3venv_dir}
 source ${py3venv_dir}/bin/activate
-pip freeze
+echo "using virtual env: $(which python)"
+read -p "if that doesn't look right you should probably control-c outta here, otherwise press enter to continue"
 pip install -r requirements.txt
